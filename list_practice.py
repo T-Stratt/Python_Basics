e9c0eb5d3fa6362534f103fc6289e63be104b88d
T-Stratt <Thomas.Wayne.Stratton@outlook.com>
2020-07-21 15:12:06 -0400

    List Project

---
+++ list_practice.py
@@ -0,0 +1,10 @@
+
+integers = [1, 2, 4, 8, 16] # a list of integers
+print(integers)
+integers.append(32) # adds 32 to end of list
+print(integers)
+integers.append(64) # adds 64 to end of list
+print(integers)
+
+integers.pop() # removes 64 to end of list
+print(integers)
